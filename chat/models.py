efe7b68bf4c5ec14cc2bee8aa740eafeaba76ed8
ErencanYldrm <96621237+ErencanYldrm@users.noreply.github.com>
2023-02-22 19:12:03 +0300

    Real Time Chat App with Django

---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Message(models.Model):
+    value = models.CharField(max_length=3000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000)
+    room = models.CharField(max_length=1000)
